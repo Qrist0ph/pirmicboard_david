81a02953cf1d9ff581def272112ca6fb510c0ee2
Christoph Außem <chris.aussem4@gmail.com>
2023-09-01 10:32:16 +0200

    Update tmp_main_works_on_gpstracker.py

--- unittests/7080gconnect/tmp_main_works_on_gpstracker.py
+++ unittests/7080gconnect/tmp_main_works_on_gpstracker.py
@@ -238,4 +238,252 @@ def mqttPublish(ccid,data,runde):
         print("published")
         
         
-        # verbindung beende
+        # verbindung beenden
+        uart.write( 'AT+SMDISC\r\n'.encode() )
+        utime.sleep(2)
+        wdt.feed()
+        print(waitResp_info())
+
+#Send AT command
+def sendAt(cmd,back,timeout=1000):
+    #rec_buff = b''
+    uart.write((cmd+'\r\n').encode())
+    utime.sleep(2)
+    wdt.feed()
+    recBuff = waitResp_info()
+    
+    if back in recBuff :
+        print("ret 1 " + recBuff+" --")
+        return 1   
+    else:
+        print("ret 0 " + recBuff+" --")
+        return 0
+
+
+def checkNetwork():
+    print("checkNetwork")
+    # https://m2msupport.net/m2msupport/atcfun-set-phone-functionality/
+    # =1 auf volle FunktionalitÃ¤t setzen
+    wdt.feed()
+    sendAt("AT+CFUN=0","OK")
+   
+    #utime.sleep(5)
+    #wdt.feed()
+    #utime.sleep(15)
+    #wdt.feed()
+    
+    #https://www.twilio.com/docs/iot/supersim/cellular-modem-knowledge-base/simcom-supersim
+    #If you wish to limit comms to LTE only, i.e., no GSM, send AT+CNMP=38.
+    sendAt("AT+CNMP=38","OK")      #Select LTE mode
+
+    #https://www.twilio.com/docs/iot/supersim/cellular-modem-knowledge-base/simcom-supersim
+    #Set the modemâ€™s Radio Access Technology (RAT) preference to Cat-M1 when you are connecting to LTE. To do so, issue AT+CMNB=1.
+    sendAt("AT+CMNB=2","OK")       #Select NB-IoT mode,if Cat-Mï¼Œplease set to 1
+ 
+    wdt.feed()
+    utime.sleep(2)
+    wdt.feed()
+    
+    sendAt("AT+CFUN=1","OK")
+    utime.sleep(2)
+    wdt.feed()
+        
+    for i in range(1, 10):
+        if sendAt("AT+CGATT?", "+CGATT: 1"):
+            print('------SIM7080G is online------\r\n')
+            break
+        else:
+            print('------SIM7080G is offline, please wait...------\r\n')
+            utime.sleep(2)
+            wdt.feed()
+            continue
+        
+    #AT+CSQ AT command returns the signal strength of the device.
+    #https://m2msupport.net/m2msupport/atcsq-signal-quality/
+    sendAt("AT+CSQ","OK")
+
+    #AT+CPSI command is used to return UE system information.
+    #https://m2msupport.net/m2msupport/atcpsi-inquiring-ue-system-information/
+    sendAt("AT+CPSI?","OK")
+    wdt.feed()
+
+    #AT+COPS AT command forces the mobile terminal to select and register the GSM/UMTS/EPS network.
+    #The device returns the list of networks that are avaialble to connect.
+    #https://m2msupport.net/m2msupport/atcops-plmn-selection/
+    sendAt("AT+COPS?","OK")
+
+    #Verify that APN is assigned using  the AT+CGNAPN comman
+    sendAt("AT+CGNAPN","OK")
+    wdt.feed()
+
+    #Activate a data connection: AT+CNACT=0,1
+    sendAt('AT+CNACT=0,1','OK')
+    #Optionally, obtain an IP address and the current connection status with
+    sendAt('AT+CNACT?','OK')
+    wdt.feed()
+    #sendAt("AT+CPSI?","OK")
+    
+    
+# geht bei jedem 2ten durchlauf in den deepsleep modus
+def deepSleepWeiche(status):
+    if ("deepsleep" in os.listdir()):
+        try:
+            os.remove('deepsleep')
+        except:
+            print("except")
+        led.off()
+        print("state: pico led aus und modem aus und picosleep")
+        print("assertModemOff() --> Power Off")
+        assertModemOff()
+        
+        print("picosleep "+str(status["sleeptime"]))
+        utime.sleep(2)
+        #hier die deepsleep zeit anpassen
+        #picosleep.seconds(30)
+        #picosleep.seconds(int(status["sleeptime"]))
+        #machine.deepsleep(int(status["sleeptime"])*1000)
+        machine.deepsleep(60000)
+        print("machine reset and dann wieder normal")
+        utime.sleep(2)
+        machine.reset()
+    else:
+        print("state: normal")
+        try:
+            os.mkdir('deepsleep')            
+        except:
+            print("except mkdir")
+
+
+def getWiFis():
+    wlan_sta = network.WLAN(network.STA_IF)
+    wlan_sta.active(True)
+    networks = wlan_sta.scan()
+    neunetworks=[]
+    print(networks)
+    for mynetwork in networks:
+        neunetwork=[mynetwork[0],ubinascii.hexlify(mynetwork[1]).decode()]
+        neunetworks.append(neunetwork)
+        #mynetwork[1]=ubinascii.hexlify(mynetwork[1]).decode()
+    return neunetworks
+    
+    
+def getCCID():
+    #Get CCID
+    uart.write("AT+CCID\r\n".encode())
+    utime.sleep(2)
+    wdt.feed()
+    ccidlong = waitResp_info()
+    c=0
+    ccid=""
+    while True:
+        c=c+1
+        if(c>10):
+            break
+        try:
+            ccid = ccidlong.splitlines()[2]
+            break
+        except:
+            utime.sleep(2)
+            wdt.feed()
+            
+    return str(ccid)
+
+
+       
+#######################################
+#on off loop
+while True:
+    machine.Pin(16, machine.Pin.OUT).value(1)
+    print("pico has just started")
+    print("Sleep 10s zum start")   
+    utime.sleep(10)
+    machine.Pin(16, machine.Pin.OUT).value(0)
+    print("PICO LED on")
+    led.on()      
+    
+    status=readStatus()
+    
+    print("lastwifis")   
+    print(status['lastwifis'])    
+    
+    print("Enable WatchDogTimer Dummy")
+    wdt = WdtDummy()
+    print("Running deepSleepWeiche")
+    #deepSleepWeiche(status)
+ 
+    print("Enable WatchDogTimer")
+    wdt = WDT(timeout=8000)
+    
+    runde=logCall()
+    # Neue Batteriespar Logik
+    # falls wifis seit 5 loops beim letzen loop ==> hat sich nicht beweget
+      # sleep variable auf lang setzen , annotaton auf stillstand
+    # sonst
+     #sleep variable auf kurz seten
+    
+    
+  
+    
+    #wifis in der Umgebgug
+    currentWifis=getWiFis()
+    
+    print("++++check sleept time "+str(currentWifis))
+    print("")
+    print(str(status["lastwifis"]))
+    print("")
+    print(currentWifis)
+    print("")
+    status["sleeptime"] = 100
+    status["notmoving"]=False
+    # falls die wifis sich nicht verändert haben sleeptime verdoppeln
+    if (str(currentWifis) == str(status["lastwifis"]) ):
+        print("++++double sleep")
+        status["sleeptime"] = min(status["sleeptime"] * 2, 3600)
+        status["notmoving"]=True
+    #hier kommt die eigentliche Logik
+    woerterbuch={'time':utime.localtime()}
+    woerterbuch["wifis"]= currentWifis
+    status["lastwifis"]=currentWifis    
+    writeStatus(status)
+    
+    print("assertModemOn --> Power On")
+    assertModemOn()    
+
+    #check ob Modem wirklich an
+    print("Sending AT to check if Modem is on")
+    utime.sleep(2)
+    wdt.feed()
+    uart.write( 'AT\r\n'.encode() )
+    utime.sleep(2)
+    wdt.feed()
+    print(waitResp_info()  )
+        
+
+    
+    ccid = getCCID();
+    print('CCID: '+ccid)
+    
+    uart.write("AT+CBC\r\n".encode())
+    utime.sleep(2)
+    wdt.feed()
+    battery = waitResp_info()
+    woerterbuch["battery"]=battery
+    print('battery: '+battery)
+    
+  
+    
+    checkNetwork()
+    
+    woerterbuch["sleeptime"]=status["sleeptime"]
+    woerterbuch["notmoving"]=status["notmoving"]
+    mqttPublish(ccid,woerterbuch,runde)
+    
+    
+    print("Machine reset and dann picosleep")
+    utime.sleep(2)
+    wdt.feed()
+    machine.reset()
+
+
+
+
