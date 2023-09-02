97794f0c7ebe74504859f1ed652b451b9ba0439d
Christoph Außem <chris.aussem4@gmail.com>
2023-09-02 08:39:04 +0200

    # 2.9.23 Mic Running

--- unittests/microphone/main.py
+++ unittests/microphone/main.py
@@ -1,13 +1,15 @@
 # Bibliotheken laden
+# 2.9.23 Mic Running
 from machine import Pin, ADC
 from time import sleep
 
-sensor = ADC(3)
+sensor = ADC(Pin(3))
 
 # Wiederholung (Endlos-Schleife)
 while True:
     value = sensor.read()
     print(str(value)+"."*int(value/10))
     sleep(0.03)
+
 
 
