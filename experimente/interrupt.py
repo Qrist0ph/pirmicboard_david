8e9933c020537573759afc167332ad126889e09d
Christoph Außem <chris.aussem4@gmail.com>
2023-09-07 16:35:58 +0200

    pir interrupts

--- experimente/interrupt.py
+++ experimente/interrupt.py
@@ -3,6 +3,7 @@ from machine import Pin, PWM
 import time
 from machine import RTC
 
+
 rtc = RTC()
 rtc.datetime((2022, 7, 25, 1, 1, 30, 42, 0)) # set date and time
 
@@ -24,13 +25,28 @@ async def increment_counter():
         # set the frequency of the buzzer to 500Hz
         pwm_c.freq(500)
         # wait for 5 seconds
-        time.sleep(5)
+        time.sleep(2)
         # stop the buzzer
         pwm_c.deinit()
-        await asyncio.sleep(60)  # Warte 60 Sekunden
+        await asyncio.sleep(10)  # Warte 60 Sekunden
+
 
 # Starte den Thread
 loop = asyncio.get_event_loop()
 loop.create_task(increment_counter())
-loop.run_forever()
+#loop.run_forever()
+#_thread.start_new_thread(my_thread, ())
 
+def callback(pin):
+    print("Pin change detected:", pin)
+
+pir_pin = Pin(10, Pin.IN)
+pir_pin.irq(trigger=Pin.IRQ_RISING, handler=callback)
+
+
+while True: 
+    value = pir_pin.value() 
+    print("PIR Value: ",value  )    
+    time.sleep(0.3)
+
+ 
