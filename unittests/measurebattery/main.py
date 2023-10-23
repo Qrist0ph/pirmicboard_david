07430b7cc2fc535f427abd0b775588f529129f1e
Christoph Außem <chris.aussem4@gmail.com>
2023-10-23 10:56:33 +0200

    checking if USB cable is connected for power supply

--- unittests/measurebattery/main.py
+++ unittests/measurebattery/main.py
@@ -1,13 +1,29 @@
+# checking if USB cable is connected for power supply
+# green led on when connected
+# red led on when disconnected
 from machine import Pin, ADC
 from time import sleep
 
-sensor = ADC(Pin(12))
-
+sensor = ADC(Pin(5))
+green = Pin(15, Pin.OUT)
+red = Pin(16, Pin.OUT)
 # Wiederholung (Endlos-Schleife)
+
+    
 while True:
-    value = sensor.read()
-    print(str(value)+"."*int(value/10))
+    vinValue = sensor.read()
+    voltage = vinValue / 4095.0 * 3.3
+    print(str(voltage))
+    if voltage==3.3:
+        green.off()
+        red.on()
+        print("cable connected")
+    else:
+        green.on()
+        red.off()
     sleep(0.5)
+
+
 
 
 
