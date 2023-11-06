3e9da4253a7a6cba3f83ab2c72342ad6b0fa171d
Christoph Außem <chris.aussem4@gmail.com>
2023-11-06 08:35:56 +0100

    battery check on IO13

--- unittests/measurebattery/main.py
+++ unittests/measurebattery/main.py
@@ -1,28 +1,19 @@
-# checking if USB cable is connected for power supply
-# assumption 3.3 means connected
-# green led on when connected
-# red led on when disconnected
+# checking battery voltage on IO13
 from machine import Pin, ADC
 from time import sleep
 
-sensor = ADC(Pin(5))
-green = Pin(15, Pin.OUT)
-red = Pin(16, Pin.OUT)
-# Wiederholung (Endlos-Schleife)
+sensor = ADC(Pin(13))
 
-    
 while True:
     vinValue = sensor.read()
     voltage = vinValue / 4095.0 * 3.3
     print(str(voltage))
-    if voltage==3.3:
-        green.off()
-        red.on()
-        print("cable connected")
-    else:
-        green.on()
-        red.off()
     sleep(0.5)
+
+
+
+
+
 
 
 
